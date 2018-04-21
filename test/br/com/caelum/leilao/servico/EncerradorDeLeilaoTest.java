37df10ee2922bfdf8743edd27ee654d37abd48eb
Ronaldo Akio <roni.217@gmail.com>
2018-04-21 20:37:23 -0300

    Criado primeiro test com mock

---
+++ test/br/com/caelum/leilao/servico/EncerradorDeLeilaoTest.java
@@ -0,0 +1,37 @@
+package br.com.caelum.leilao.servico;
+
+import java.util.Arrays;
+import java.util.Calendar;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import br.com.caelum.leilao.builder.CriadorDeLeilao;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.infra.dao.LeilaoDao;
+
+public class EncerradorDeLeilaoTest {
+
+	@Test
+	public void EncerradorDeLeilaio() {
+		Calendar antiga = Calendar.getInstance();
+		antiga.set(1999, 1, 20);
+
+		Leilao leilao1 = new CriadorDeLeilao().para("TV de plasma").naData(antiga).constroi();
+		Leilao leilao2 = new CriadorDeLeilao().para("Geladeira").naData(antiga).constroi();
+
+		LeilaoDao daoFalso = mock(LeilaoDao.class);
+
+		when(daoFalso.correntes()).thenReturn(Arrays.asList(leilao1, leilao2));
+
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(daoFalso);
+		encerrador.encerra();
+
+		assertEquals(2, encerrador.getTotalEncerrados());
+		assertTrue(leilao1.isEncerrado());
+		assertTrue(leilao2.isEncerrado());
+	}
+}
